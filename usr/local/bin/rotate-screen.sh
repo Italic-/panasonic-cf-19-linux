37c8bb5732deb2acbd3d946bdc1351f73c6027b8
italic <Italic-@users.noreply.github.com>
2017-07-20 23:08:17 -0700

    Modify for CF-19 Mk3
    Also simplify code for easy modification.

--- usr/local/bin/rotate-screen.sh
+++ usr/local/bin/rotate-screen.sh
@@ -7,7 +7,6 @@
 
 #### configuration
 # find your Touchscreen  device with `xinput`
-TouchscreenDevice='Fujitsu Component USB Touch Panel'
 
 if [ "$1" = "--help"  ] || [ "$1" = "-h"  ] ; then
 echo 'Usage: rotate-screen.sh [OPTION]'
@@ -15,12 +14,23 @@ echo
 echo 'This script rotates the screen and touchscreen input 90 degrees each time it is called,'
 echo 'also disables the touchpad, and enables the virtual keyboard accordingly'
 echo
-echo Usage:
+echo Available Flags:
 echo ' -h --help display this help'
-echo ' -j (just horizontal) rotates the screen and touchscreen input only 180 degrees'
 echo ' -n always rotates the screen back to normal'
+echo ' -f (just horizontal) rotates the screen and touchscreen input only 180 degrees'
+echo ' -l rotates the screen and touchscreen input 90 degrees counter-clockwise'
+echo ' -r rotates the screen and touchscreen input 90 degrees clockwise'
 exit 0
 fi
+
+TouchscreenDevice='Virtual core pointer'
+DeviceList=(
+    'Virtual core pointer'
+    'Wacom ISDv4 93 Pad'
+    'Wacom ISDv4 93 Pen stylus'
+    'Wacom ISDv4 93 Pen eraser'
+    'PS/2 Generic Mouse'
+)
 
 screenMatrix=$(xinput --list-props "$TouchscreenDevice" | awk '/Coordinate Transformation Matrix/{print $5$6$7$8$9$10$11$12$NF}')
 
@@ -31,47 +41,67 @@ screenMatrix=$(xinput --list-props "$TouchscreenDevice" | awk '/Coordinate Trans
 normal='1 0 0 0 1 0 0 0 1'
 normal_float='1.000000,0.000000,0.000000,0.000000,1.000000,0.000000,0.000000,0.000000,1.000000'
 
-#⎡ -1  0 1 ⎤
-#⎜  0 -1 1 ⎥
-#⎣  0  0 1 ⎦
+# Inverted (180°)
+# ⎡ -1  0 1 ⎤
+# ⎜  0 -1 1 ⎥
+# ⎣  0  0 1 ⎦
 inverted='-1 0 1 0 -1 1 0 0 1'
 inverted_float='-1.000000,0.000000,1.000000,0.000000,-1.000000,1.000000,0.000000,0.000000,1.000000'
 
-# 90° to the left
+# 90° counter-clockwise
 # ⎡ 0 -1 1 ⎤
 # ⎜ 1  0 0 ⎥
 # ⎣ 0  0 1 ⎦
 left='0 -1 1 1 0 0 0 0 1'
 left_float='0.000000,-1.000000,1.000000,1.000000,0.000000,0.000000,0.000000,0.000000,1.000000'
 
-# 90° to the right
-#⎡  0 1 0 ⎤
-#⎜ -1 0 1 ⎥
-#⎣  0 0 1 ⎦
+# 90° clockwise
+# ⎡  0 1 0 ⎤
+# ⎜ -1 0 1 ⎥
+# ⎣  0 0 1 ⎦
 right='0 1 0 -1 0 1 0 0 1'
 
-if [ $screenMatrix == $normal_float ] && [ "$1" != "-n" ]
-then
-  echo "90° to the right"
-  xrandr -o right
-  xinput set-prop "$TouchscreenDevice" 'Coordinate Transformation Matrix' $right
-  sleep 1
-  onboard &
-elif [ $screenMatrix == $inverted_float ] && [ "$1" != "-j" ] && [ "$1" != "-n" ]
-then
-  #this is not desired on Panasonic CF-19, code left in if somebody still wants it
-  echo "Upside down"
-  xrandr -o inverted
-  xinput set-prop "$TouchscreenDevice" 'Coordinate Transformation Matrix' $inverted
-elif [ $screenMatrix == $left_float ] && [ "$1" != "-j" ] && [ "$1" != "-n" ]
-then
-  echo "Back to normal"
-  xrandr -o normal
-  xinput set-prop "$TouchscreenDevice" 'Coordinate Transformation Matrix' $normal
-  killall onboard
-else
-  echo "90° to the left"
-  xrandr -o left
-  xinput set-prop "$TouchscreenDevice" 'Coordinate Transformation Matrix' $left
-fi
+function rot {
+    for device in "${DeviceList[@]}"; do
+        xinput set-prop "$device" 'Coordinate Transformation Matrix' $1
+    done
+}
 
+if [[ "$1" == "-r" || \
+   ( $screenMatrix == $normal_float && \
+   "$1" != "-f" && \
+   "$1" != "-l" && \
+   "$1" != "-n" ) ]]; then
+    echo "90° to the right"
+    xrandr -o right
+    rot "$right"
+    killall xvkbd
+    xvkbd &
+# elif [ $screenMatrix == $right_float ] && \
+#      [ "$1" != "-n" ] || \
+#      [ "$1" == "-f" ]; then
+#     # This is not desired on Panasonic CF-19, code left in if somebody still wants it
+#     echo "Upside down"
+#     xrandr -o inverted
+#     rot "$inverted"
+elif [[ "$1" == "-n" || \
+     ( $screenMatrix == $left_float && \
+     "$1" != "-f" && \
+     "$1" != "-l" && \
+     "$1" != "-r" ) ]]; then
+    echo "Back to normal"
+    killall xvkbd
+    xrandr -o normal
+    rot "$normal"
+elif [[ "$1" == "-l" || \
+     ( $screenMatrix == $right_float && \
+     "$1" != "-f" && \
+     "$1" != "-r" && \
+     "$1" != "-n" ) || \
+     ! "$1" ]]; then
+    echo "90° to the left"
+    xrandr -o left
+    rot "$left"
+    killall xvkbd
+    xvkbd &
+fi
--- usr/local/bin/toggle-osk.sh
+++ usr/local/bin/toggle-osk.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-# This script toggles the appearance of the 'onboard' on-screen keyboard utility.
-# Please make sure the filename of this script does not contain the 'onboard' string.
+# This script toggles the appearance of the 'xvkbd' on-screen keyboard utility.
+# Please make sure the filename of this script does not contain the 'xvkbd' string.
 # For Panasonic Toughbook CF-19, to use with tablet buttons, provided by the modified panasonic-hbtn driver
 
-if pgrep "onboard" > /dev/null 2>&1
+if pgrep "xvkbd" > /dev/null 2>&1
 then
-    echo "onboard running, killing"
-    killall onboard
+    killall xvkbd
 else
-#    echo "onboard not running, starting"
-    onboard &
+    xvkbd &
 fi
